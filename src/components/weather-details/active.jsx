a45129012e767cc727c48474b633e3775e633e11
vsharp2 <vsharp2@thezenith.com>
2019-05-09 14:10:36 -0700

    fixed styling of top section

--- src/components/weather-details/active.jsx
+++ src/components/weather-details/active.jsx
@@ -13,9 +13,10 @@ export default class ActiveForecast extends React.Component {
             <h2 className="weather__location text--center">
                 {locationData.name}
             </h2>
-          <div className="weather--box flex--row">
-                <section className="weather--box__main-content container--flex flex--col">
-                    <p>Daily Average</p>
+          <div className="weather--box row">
+                <div className="lg-5">&nbsp;</div>
+                <section className="weather--box__main-content lg-8">
+                    <h3 className="text--bold">Daily Average</h3>
                     <div className="weather--main-content">
                         <div className="weather__img">
                             <img src={activeViewingData.day.condition.icon}
@@ -28,20 +29,21 @@ export default class ActiveForecast extends React.Component {
                     </div>
                     <div className="weather__details">{activeViewingData.day.condition.text}</div>
                 </section>
-                <section className="weather--box__details-content flex--col">
+                <section className="weather--box__details-content sm-25 lg-6">
                     <h3>Forecast Details</h3>
                     <ul className="list--unstyled">
-                        <li className="list__item text--left">
+                        <li className="list__item text--center">
                             <p><b>High of</b>: {activeViewingData.day.maxtemp_f}&deg;</p>
                         </li>
-                        <li className="list__item text--left">
+                        <li className="list__item text--center">
                             <p><b>Low of</b>: {activeViewingData.day.mintemp_f}&deg;</p>
                         </li>
-                        <li className="list__item text--left">
+                        <li className="list__item text--center">
                             <p><b>Average Humidity</b>: {activeViewingData.day.avghumidity}&deg;</p>
                         </li>
                     </ul>
                 </section>
+              <div className="lg-6">&nbsp;</div>
             </div>
         </section>
     }
